5f4f19fc512f9fbb9eceb9e515909ee1bcfc407f
Dylan Travis <dylan.travis23@outlook.com>
2021-10-14 15:54:50 -0700

    card ui

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -3,21 +3,30 @@ import React from "react";
 const Card = ({ project }) => {
   return (
     <div className="card-container">
-      <img className="card-image" src={project.images[1]} />
       <div className="card-content">
-        <h3 className="card-name">{project.name}</h3>
-        <p className="card-text">{project.description}</p>
+        <img className="card-image" src={project.images[1]} />
+        <div style={{ padding: "15px" }}>
+          <h3 className="card-name">{project.name}</h3>
+          <p className="card-text">{project.description}</p>
+        </div>
       </div>
-      <p className="card-tech-header">Tech-Stack</p>
-      <div className="card-stack">
-        {project.skills.map((skill) => {
-          return <div className="card-tech">{skill}</div>;
-        })}
-      </div>
-      <div className="card-buttons-container">
-        <div className="card-button">Github</div>
-        <div className="card-button">Learn More</div>
-        <div className="card-button">Live Demo</div>
+
+      <div className="card-content-end">
+        <p className="card-tech-header">Tech stack</p>
+        <div className="card-stack">
+          {project.skills.map((skill, index) => {
+            return (
+              <div key={index} className="card-tech">
+                {skill}
+              </div>
+            );
+          })}
+        </div>
+        <div className="card-buttons-container">
+          <div className="card-button">Github</div>
+          <div className="card-button">Learn More</div>
+          <div className="card-button">Live Demo</div>
+        </div>
       </div>
     </div>
   );
