2a5d1c24cafe6779d4a58d9d89a02a1189d941e2
Dylan Travis <dylan.travis23@outlook.com>
2021-10-19 16:58:14 -0700

    mobile nav slide

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,63 +1,114 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Socials from "./Socials";
-import { DiCode } from "react-icons/di";
 import { Link } from "react-scroll";
 
 const Header = () => {
+  const [active, setActive] = useState(false);
   return (
-    <div className="header-container">
-      <div className="header-name">
-        {/* <DiCode className="logo"/> */}
-        <Link
-          to="hero"
-          spy={true}
-          smooth={true}
-          duration={1500}
-          // offset={-60}
-          activeClass="active-link"
-          className="cursor"
-        >
-          <h5 className="section-title" style={{ fontSize: "2rem" }}>
-            Dylan Travis
-          </h5>
-        </Link>
+    <div>
+      <div className="header-container">
+        <div className="header-name">
+          <Link
+            to="hero"
+            spy={true}
+            smooth={true}
+            duration={1500}
+            // offset={-60}
+            activeClass="active-link"
+            className="cursor"
+          >
+            <h5 className="section-title" style={{ fontSize: "2rem" }}>
+              Dylan Travis
+            </h5>
+          </Link>
+        </div>
+        <div className="header-navigation-container">
+          <div className="header-navigation">
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              duration={1500}
+              // offset={-60}
+              activeClass="active-link"
+              className="cursor"
+            >
+              Projects
+            </Link>
+            <Link
+              to="technologies"
+              spy={true}
+              smooth={true}
+              duration={1500}
+              // offset={-60}
+              activeClass="active-link"
+              className="cursor"
+            >
+              Tech
+            </Link>
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              duration={1500}
+              // offset={-60}
+              activeClass="active-link"
+              className="cursor"
+            >
+              About Me
+            </Link>
+          </div>
+          <Socials />
+        </div>
+
+        <div onClick={() => setActive(!active)} className="burger-container">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
       </div>
-      <div className="header-navigation">
-        <Link
-          to="projects"
-          spy={true}
-          smooth={true}
-          duration={1500}
-          // offset={-60}
-          activeClass="active-link"
-          className="cursor"
-        >
-          Projects
-        </Link>
-        <Link
-          to="technologies"
-          spy={true}
-          smooth={true}
-          duration={1500}
-          // offset={-60}
-          activeClass="active-link"
-          className="cursor"
-        >
-          Technologies
-        </Link>
-        <Link
-          to="about"
-          spy={true}
-          smooth={true}
-          duration={1500}
-          // offset={-60}
-          activeClass="active-link"
-          className="cursor"
-        >
-          About Me
-        </Link>
+      <div className={`mobile-menu ${active && "mobile-menu-active"}`}>
+        <div className="mobile-menu-container">
+          <Link
+            to="projects"
+            spy={true}
+            smooth={true}
+            duration={1500}
+            // offset={-60}
+            activeClass="active-link"
+            className="cursor mobile-link"
+          >
+            Projects
+          </Link>
+          <Link
+            to="technologies"
+            spy={true}
+            smooth={true}
+            duration={1500}
+            // offset={-60}
+            activeClass="active-link"
+            className="cursor mobile-link"
+          >
+            Tech
+          </Link>
+          <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={1500}
+            // offset={-60}
+            activeClass="active-link"
+            className="cursor mobile-link"
+          >
+            About Me
+          </Link>
+
+          <Socials />
+          <p className="mobile-link" onClick={() => setActive(false)}>
+            close
+          </p>
+        </div>
       </div>
-      <Socials />
     </div>
   );
 };
