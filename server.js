666f5416f35c61b812cdd1acd6f8bcddbf111ce7
Dylan Travis <dylan.travis23@outlook.com>
2021-11-21 22:57:51 -0800

    .

--- server.js
+++ server.js
@@ -20,24 +20,6 @@ mongoose
 //middleware
 app.use(express.json());
 app.use(cors());
-
-const whitelist = [
-  "http://localhost:3000",
-  "https://dylantravis.herokuapp.com/api",
-];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     console.log("** Origin of request " + origin);
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       console.log("Origin acceptable");
-//       callback(null, true);
-//     } else {
-//       console.log("Origin rejected");
-//       callback(!new Error("Not allowed by CORS "));
-//     }
-//   },
-// };
-app.use(cors());
 app.use("/api", authRoutes);
 
 // need for production
