d4db73b49b717c8de30b023216bd0b3777c65531
Dylan Travis <dylan.travis23@outlook.com>
2021-11-21 23:43:43 -0800

    rmvd unifedTop

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const app = express();
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
   })
   .then(() => console.log("DB is connected"));
 
