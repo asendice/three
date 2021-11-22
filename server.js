a4b84952775b67e0318aecf72d94e1045a82a435
Dylan Travis <dylan.travis23@outlook.com>
2021-11-21 23:16:06 -0800

    cors

--- server.js
+++ server.js
@@ -35,6 +35,20 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(!new Error("Not allowed by CORS "));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
